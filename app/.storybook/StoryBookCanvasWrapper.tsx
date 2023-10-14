ca55804ad7b5ebc1cabf72faa3fb8c17a8091cac
Ruben van der Leun <ruben@leunix.nl>
2023-10-15 01:19:56 +0200

    ➕ added @react-three/xr and implemented in Storybook

--- app/.storybook/StoryBookCanvasWrapper.tsx
+++ app/.storybook/StoryBookCanvasWrapper.tsx
@@ -1,33 +1,48 @@
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { BackSide } from 'three';
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
-import background from './holodeck-pattern.jpg';
-import { useRef } from 'react';
-import * as TWEEN from '@tweenjs/tween.js';
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { BackSide } from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+import background from "./holodeck-pattern.jpg";
+import * as TWEEN from "@tweenjs/tween.js";
+import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 
 const Environment = ({ children }) => {
-    const map = useLoader(TextureLoader, background);
+  const map = useLoader(TextureLoader, background);
 
-    useFrame(() => {
-        TWEEN.update();
-    });
+  useFrame(() => {
+    TWEEN.update();
+  });
 
-    return                 <><OrbitControls />
-    <mesh scale={[1050, 1050, 1050]}>
+  return (
+    <XR>
+      <Hands  />
+      <Controllers />
+      <OrbitControls />
+      <ambientLight intensity={0.5} />
+      <mesh scale={[1050, 1050, 1050]}>
         <boxGeometry />
         <meshBasicMaterial map={map} side={BackSide} />
-    </mesh>
-    {children}</>
-
-}
+      </mesh>
+      {children}
+    </XR>
+  );
+};
 
 export const StoryBookCanvasWrapper = ({ children }) => {
-    return (
-        <div style={{ position: 'fixed', width: '100vw', height: '100vh', top: 0, left: 0 }}>
-            <Canvas>
-                <Environment>{ children }</Environment>
-            </Canvas>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <VRButton />
+      <Canvas>
+        <Environment>{children}</Environment>
+      </Canvas>
+    </div>
+  );
+};
